083c5f9a6ab70a327b838833a815252821272309
Kevin7744 <kevinkipkoech@kabarak.ac.ke>
2023-03-17 00:12:18 -0700

    Initial commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 /**
- * main  - Entry point
- *
- *Return : Alwasys 0 (Success)
+ * main - prints the size of various types on the computer it is compiled and
+ * run on.
+ * Return: 0
  */
 int main (void)
 {
-    char a;
-    int b;
-    long int c;
-    long long int d;
-    float f;
-
-    printf("size of char: %lu byte(s)\n", sizeof(a));
-    printf("size of int: %lu byte(s)\n", sizeof(b));
-    printf("size of long int: %lu byte(s)\n", sizeof(c));
-    printf("size of long long int: %lu byte(s)\n", sizeof(d));
-    printf("size of float: %lu byte(s)\n", sizeof(f));
+    printf("size of char: %lu byte(s)\n", sizeof(char));
+    printf("size of int: %lu byte(s)\n", sizeof(int));
+    printf("size of long int: %lu byte(s)\n", sizeof(long int));
+    printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
+    printf("size of float: %lu byte(s)\n", sizeof(float));
     return 0;
+
 }
