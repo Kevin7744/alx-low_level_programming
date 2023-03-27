5fdf6f3ced4caa0c6709eac806ca54105647aea9
Kevin7744 <kevinkipkoech@kabarak.ac.ke>
2023-03-27 07:00:33 -0700

    C file changes

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print-rev - prints a string, in reverse, followed by a new line
+ * @s: string to be printed
+ * Return: void
+*/
+void print_rev(char *s)
+{
+    int i = 0;
+    int o;
+
+    while (*s !='\0')
+    {
+        i++;
+        s++;
+    }
+    s--;
+    for (o = i; o >0; o--)
+    {
+        _putchar(*s);
+        s--;
+    }
+    _putchar('\n');
+}
