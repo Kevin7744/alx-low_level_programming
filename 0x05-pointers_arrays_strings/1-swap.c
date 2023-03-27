3bb750c23b99b3025f5be7f2ddd557a331dab8ca
Kevin7744 <kevinkipkoech@kabarak.ac.ke>
2023-03-27 02:56:59 -0700

    Initil commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two intergers
+ * @a: first interger
+ * @b: second interger
+ * return: void
+*/
+void swap_int(int *a, int *b)
+{
+    int p;
+
+    p = *a;
+    *a = *b;
+    *b = p;
+}
