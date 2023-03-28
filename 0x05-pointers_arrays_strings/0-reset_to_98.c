09ba6bb50d0bc9cb331c124eec0301c316e45fde
Kevin7744 <kevinkipkoech@kabarak.ac.ke>
2023-03-28 07:20:18 -0700

    Quiz 0 fixe

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * reset_to_98 - a function that takes a pointer to an int as parameter
- * and updates the value it points to to 98
+ * reset_to_98 - a function that takes a pointer as parameter
+ * and updates the value it points to 98
  * @n: input
  * Return: n
  */
