81ab97d3df2a95e5853f51ce17fe5de46b99a0bf
Kevin7744 <kevinkipkoech@kabarak.ac.ke>
2023-05-01 05:12:52 -0700

    Initial vommit on more singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -50,3 +50,40 @@ size_t looped_listint_len(const listint_t *head)
  }
 
  return (0);
+}
+
+/**
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+ size_t nodes, index = 0;
+
+ nodes = looped_listint_len(head);
+
+ if (nodes == 0)
+ {
+ for (; head != NULL; nodes++)
+ {
+ printf("[%p] %d\n", (void *)head, head->n);
+ head = head->next;
+ }
+ }
+
+ else
+ {
+ for (index = 0; index < nodes; index++)
+ {
+ printf("[%p] %d\n", (void *)head, head->n);
+ head = head->next;
+ }
+
+ printf("-> [%p] %d\n", (void *)head, head->n);
+ }
+
+ return (nodes);
+}
+
