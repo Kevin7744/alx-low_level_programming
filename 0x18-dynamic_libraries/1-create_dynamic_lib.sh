ecdd2423d217557344a853d38193240cab53f7d7
Kevin7744 <arapbiisubmissions@gmail.com>
2023-10-20 08:45:21 -0700

    Task crrection

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -Wextra -werror -pendatic -c -fPIC *.c
+gcc -Wall -Wextra -Werror -pendatic -c -fPIC *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
