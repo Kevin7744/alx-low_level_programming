d57b3823ffb4fa2e6dd535bc83a4e6b63bab11b9
Kevin7744 <arapbiisubmissions@gmail.com>
2023-10-20 09:14:08 -0700

    Task 0 correction

--- 0x18-dynamic_libraries/1-isdigit.c
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -1,16 +1,12 @@
 #include "main.h"
-
 /**
- * _isdigit - check if numbers are 0 - 9
+ * _isdigit - checks for numbers from 0 - 9
  * @c: char to be checked
  *
- * Return: 0 or 1
+ * Return: returns 1 if c is a digit 0 if otherwise
  */
 
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
+	return(c >= '0' && c <= '9');
 }
--- 0x18-dynamic_libraries/6-abs.c
+++ 0x18-dynamic_libraries/6-abs.c
@@ -1,16 +1,17 @@
 #include "main.h"
-
-
-	/**
-	 * _abs - computes the absolute value of an integer
-	 * @n: the int to check
-	 * Return: the absolute value of int
-	 */
-	int _abs(int n)
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
 	{
-		if (n >= 0)
-		{
-			return (n);
-		}
-		return (-n);
+		return (n);
+	} 
+	else
+	{
+	return (n * -1);
 	}
+}
