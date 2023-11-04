c6eefcb6d4c2f64d35957d7d5c99050bc80b1a32
Kevin7744 <arapbiisubmissions@gmail.com>
2023-11-03 22:54:21 -0700

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,25 +8,25 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    unsigned int i = 0;
-    hash_table_t *ht = malloc(sizeof(hash_table_t));
+	unsigned int i = 0;
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
-    if(ht == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        return (NULL);
-    }
+	if (ht == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		return (NULL);
+	}
 
-    ht->size = size;
-    ht->array = malloc(sizeof(hash_node_t *) * size);
-    if (ht->array == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        return (NULL);
-    }
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		return (NULL);
+	}
 
-    for (; i < size; i++)
-        ht->array[i] = NULL;
+	for (; i < size; i++)
+		ht->array[i] = NULL;
 
-    return (ht);
+	return (ht);
 }
