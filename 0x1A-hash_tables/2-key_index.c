4470b333f8af684e54daae47c2ad38ec60a1291b
Kevin7744 <arapbiisubmissions@gmail.com>
2023-11-04 00:24:16 -0700

    key -> index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,7 +7,7 @@
  *
  * Return: Index number for the key/value pair in the hash table array.
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
 }
