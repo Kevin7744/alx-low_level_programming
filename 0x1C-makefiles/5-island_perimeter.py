2a46cbf2ec51e234bb70443d479914e93380ee0d
Kevin7744 <arapbiisubmissions@gmail.com>
2023-12-20 23:35:37 -0800

    Make

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Function that returns the perimeter of the island desctibed in grid:
+    grid is a liut of integers:
+        0 represents a water zone
+        1 represents a land zone
+        one cell is a square with ise length 1
+"""
+def island_perimeter(grid):
+    for 
